69ce68c7ca6a8d8b1438dfdae9fd543ed362bcb5
Lorenzo <logongas@gmail.com>
2013-03-16 10:39:38 +0100

    Depurar el build.xml en Jenkins Limpiar el Script de creación de la base de datos

--- angularjs/src/java/database/V1__seguros.sql
+++ angularjs/src/java/database/V1__seguros.sql
@@ -1,23 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               5.5.27 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2013-03-05 22:35:53
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
-
--- Dumping database structure for seguros
-DROP DATABASE IF EXISTS `seguros`;
-CREATE DATABASE IF NOT EXISTS `seguros` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `seguros`;
-
-
--- Dumping structure for table seguros.seguro
-DROP TABLE IF EXISTS `seguro`;
 CREATE TABLE IF NOT EXISTS `seguro` (
   `idSeguro` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) DEFAULT NULL,
@@ -39,13 +19,7 @@ CREATE TABLE IF NOT EXISTS `seguro` (
   PRIMARY KEY (`idSeguro`)
 ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
--- Dumping data for table seguros.seguro: ~0 rows (approximately)
-/*!40000 ALTER TABLE `seguro` DISABLE KEYS */;
-/*!40000 ALTER TABLE `seguro` ENABLE KEYS */;
 
-
--- Dumping structure for table seguros.usuario
-DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) DEFAULT NULL,
@@ -57,10 +31,6 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Dumping data for table seguros.usuario: ~1 rows (approximately)
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
 INSERT INTO `usuario` (`idUsuario`, `login`, `password`, `nombre`, `ape1`, `ape2`, `admin`) VALUES
 	(1, 'alumno', 'alumno', 'Juan', 'García', 'Perez', 1);
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-/*!40014 SET FOREIGN_KEY_CHECKS=1 */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
